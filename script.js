b966f8ac35d1ae7158dc80361693995d4245a2d9
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-04 23:57:29 +0100

    evaluates numbers inputted

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const buttons = Array.from(document.getElementsByClassName('btn-grid__item'));
 buttons.map(button => {
     button.addEventListener('click', (e) =>{
         switch(e.target.innerText){
+            
+            case 'AC': 
+                display.innerText = "";
+                break;
+
+            case '=':
+                display.innerText = eval(display.innerText);
+                break;
+            
             default:
                 display.innerText +=e.target.innerText;
         }
